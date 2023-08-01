09b74212b53503ed75b3de47e6a061b11090067e
Yurii Skrynnykov <truef1s7@gmail.com>
2023-08-01 21:03:27 +0300

    Clean-up example

--- lib/sdl/example.ex
+++ lib/sdl/example.ex
@@ -13,17 +13,17 @@ defmodule Sdl.Example do
         flags \\ [:sdl_window_shown]
       ) do
     Sdl.sdl_init([:sdl_init_everything])
+
     driver_name = Sdl.sdl_get_video_driver(0)
     Logger.info("using #{driver_name} video_driver")
 
     window = Sdl.sdl_create_window(title, x, y, w, h, flags)
-    Logger.info("window opaque struct address: #{Integer.to_string(window.ref, 16)}")
+    Logger.info("window opaque struct address: 0x#{Integer.to_string(window.ref, 16)}")
 
     renderer = Sdl.sdl_create_renderer(window, -1, [:sdl_renderer_accelerated])
     surface = Sdl.img_load(path)
-    IO.inspect(surface)
-
     texture = Sdl.sdl_create_texture_from_surface(renderer, surface)
+
     Sdl.sdl_free_surface(surface)
     Sdl.sdl_query_texture(texture)
     Sdl.sdl_render_clear(renderer)
